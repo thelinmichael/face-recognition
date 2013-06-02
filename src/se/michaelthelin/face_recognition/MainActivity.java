beadaa835ddde454bc310fd8406e9347e08bc3c7
Michael Thelin <thelinmichael@spotify.com>
2013-06-02 15:34:37 +0200

    Some logging. Attempt to get album permissions.

--- src/se/michaelthelin/face_recognition/MainActivity.java
+++ src/se/michaelthelin/face_recognition/MainActivity.java
@@ -1,16 +1,21 @@
 package se.michaelthelin.face_recognition;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
-import com.facebook.model.GraphUser;
+import com.facebook.model.GraphObject;
 
 public class MainActivity extends Activity {
 
@@ -21,29 +26,45 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    // Create session with album permissions
+
     // Start Facebook login
-    Log.i(TAG, "Opening session..");
     Session.openActiveSession(this, true, new Session.StatusCallback() {
 
       // Callback when session changes state
       @Override
       public void call(Session session, SessionState state, Exception exception) {
-        Log.i(TAG, "Session opened. Checking state. (Open -> " + session.isOpened() + ")");
+
+        // Check if token has been updated
+        if (state == SessionState.OPENED_TOKEN_UPDATED) {
+          Log.i(TAG, "Updated token");
+        }
+        
+        String[] PERMISSION_ARRAY_READ = {"email"};
+        List<String> permissionList = Arrays.asList(PERMISSION_ARRAY_READ);
+
+        // Request permissions.
+        session.requestNewReadPermissions(new Session.NewPermissionsRequest(MainActivity.this, permissionList));
         
         if (session.isOpened()) {
-          Log.i(TAG, "Session is open. Quering OpenGraph..");
-          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+          
+          // If all required permissions are available...
+          if (session.getPermissions().containsAll(permissionList)) {
+            Log.i(TAG, "Email permission avaiable.");
+            Request.executeGraphPathRequestAsync(session, "me/albums", new Request.Callback() {
 
-            @Override
-            public void onCompleted(GraphUser user, Response response) {
-              Log.i(TAG, "Completed. Got user " + user);
-              if (user != null) {
-                TextView welcome = (TextView) findViewById(R.id.welcome);
-                welcome.setText("Hello " + user.getName() + "!");
+              @Override
+              public void onCompleted(Response response) {
+                GraphObject graphObject = response.getGraphObject();
+                Map graphMap = graphObject.asMap();
+                JSONArray data = (JSONArray) graphMap.get("data");
+                Log.i(TAG, data.toString());
               }
-            }
+            });
+          } else {
+            Log.i(TAG, "Email permission was not available");
+          }
 
-          });
         }
       }
     });
@@ -52,9 +73,8 @@ public class MainActivity extends Activity {
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
-    
-    Log.i(TAG, "Got Activity result.");
-    Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    if (Session.getActiveSession() != null) {
+      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
   }
-
 }
